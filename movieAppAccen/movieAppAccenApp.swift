f78f9a3d3cb21a19702ad206086a9f3e75bd5bfc
Orlando Velasco <orvenuevo@hotmail.com>
2023-10-19 19:40:01 -0300

    Initial Commit

---
+++ movieAppAccen/movieAppAccenApp.swift
@@ -0,0 +1,17 @@
+//
+//  movieAppAccenApp.swift
+//  movieAppAccen
+//
+//  Created by Orlando Velasco on 19-10-23.
+//
+
+import SwiftUI
+
+@main
+struct movieAppAccenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
